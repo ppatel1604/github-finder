94b087d7821d2978eed6b5e03a1f542ef6af3137
Parth Patel <p.patel81@yahoo.com>
2019-12-25 10:19:37 +1300

    Fixed the icon for the alert component

--- src/components/layout/Alert.js
+++ src/components/layout/Alert.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faGgCircle } from '@fortawesome/free-brands-svg-icons';
+import { faInfoCircle } from '@fortawesome/free-solid-svg-icons';
 import PropTypes from 'prop-types';
 
 export const Alert = ({ alert }) => {
   return (
     alert != null && (
       <div className={`alert alert-${alert.type}`}>
-        <FontAwesomeIcon icon={faGgCircle} /> {alert.message}
+        <FontAwesomeIcon icon={faInfoCircle} /> {alert.message}
       </div>
     )
   );
