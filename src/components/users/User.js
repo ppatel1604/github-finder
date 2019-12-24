fc5751edb40785ff56bb8dd5c2cb5e3246869ec0
Parth Patel <p.patel81@yahoo.com>
2019-12-25 10:30:19 +1300

    Refactored User component from a class component to functional component

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,4 +1,4 @@
-import React, { Component, Fragment } from 'react';
+import React, { Fragment, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import Spinner from '../layout/Spinner';
 import { Link } from 'react-router-dom';
@@ -6,118 +6,115 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCheck, faTimesCircle } from '@fortawesome/free-solid-svg-icons';
 import { Repos } from '../repos/Repos';
 
-export class User extends Component {
-  componentDidMount() {
-    this.props.getUser(this.props.match.params.login);
-    this.props.getUserRepos(this.props.match.params.login);
-  }
+const User = ({ user, loading, repos, getUserRepos, getUser, match }) => {
+  useEffect(() => {
+    getUser(match.params.login);
+    getUserRepos(match.params.login);
+    //eslint-disable-next-line
+  }, []);
 
-  static propsTypes = {
-    loading: PropTypes.bool,
-    user: PropTypes.object.isRequired,
-    getUser: PropTypes.func.isRequired,
-    getRepos: PropTypes.func.isRequired,
-    repos: PropTypes.array.isRequired
-  };
+  const {
+    name,
+    avatar_url,
+    location,
+    bio,
+    blog,
+    login,
+    html_url,
+    followers,
+    following,
+    public_repos,
+    public_gists,
+    company,
+    hireable
+  } = user;
 
-  render() {
-    const {
-      name,
-      avatar_url,
-      location,
-      bio,
-      blog,
-      login,
-      html_url,
-      followers,
-      following,
-      public_repos,
-      public_gists,
-      company,
-      hireable
-    } = this.props.user;
-
-    const { loading, repos } = this.props;
-
-    if (loading) return <Spinner />;
-    return (
-      <Fragment>
-        <Link to='/' className='btn btn-light'>
-          Back to Search
-        </Link>
-        Hireable:{' '}
-        <FontAwesomeIcon
-          className={hireable ? 'text-success' : 'text-danger'}
-          icon={hireable ? faCheck : faTimesCircle}
-        />
-        <div className='card grid-2'>
-          <div className='all-center'>
-            <img
-              src={avatar_url}
-              className='round-img'
-              style={{ width: '150px' }}
-              alt='avatar'
-            />
-            <h1>{name}</h1>
-            <p>Location: {location}</p>
-          </div>
-          <div>
-            {bio && (
-              <Fragment>
-                <h3>Bio</h3>
-                <p>{bio}</p>
-              </Fragment>
-            )}
-            <a
-              href={html_url}
-              className='btn btn-dark my-1'
-              target='_blank'
-              rel='noreferrer noopener'
-            >
-              View Github Profile
-            </a>
-            <ul>
-              <li>
-                {login && (
-                  <Fragment>
-                    <strong>Username: </strong>
-                    {login}
-                  </Fragment>
-                )}
-              </li>
-              <li>
-                {company && (
-                  <Fragment>
-                    <strong>Company: </strong>
-                    {company}
-                  </Fragment>
-                )}
-              </li>
-              <li>
-                {blog && (
-                  <Fragment>
-                    <strong>Website: </strong>
-                    <a href={blog} target='_blank' rel='noreferrer noopener'>
-                      {blog}
-                    </a>
-                  </Fragment>
-                )}
-              </li>
-            </ul>
-          </div>
-        </div>
-        <div className='text-center'>
-          <div className='badge badge-primary'>Followers:{followers}</div>
-          <div className='badge badge-success'>following:{following}</div>
-          <div className='badge badge-light'>Public Repos:{public_repos}</div>
-          <div className='badge badge-dark'>Public Gists:{public_gists}</div>
+  if (loading) return <Spinner />;
+  return (
+    <Fragment>
+      <Link to='/' className='btn btn-light'>
+        Back to Search
+      </Link>
+      Hireable:{' '}
+      <FontAwesomeIcon
+        className={hireable ? 'text-success' : 'text-danger'}
+        icon={hireable ? faCheck : faTimesCircle}
+      />
+      <div className='card grid-2'>
+        <div className='all-center'>
+          <img
+            src={avatar_url}
+            className='round-img'
+            style={{ width: '150px' }}
+            alt='avatar'
+          />
+          <h1>{name}</h1>
+          <p>Location: {location}</p>
         </div>
         <div>
-          <Repos repos={repos}></Repos>
+          {bio && (
+            <Fragment>
+              <h3>Bio</h3>
+              <p>{bio}</p>
+            </Fragment>
+          )}
+          <a
+            href={html_url}
+            className='btn btn-dark my-1'
+            target='_blank'
+            rel='noreferrer noopener'
+          >
+            View Github Profile
+          </a>
+          <ul>
+            <li>
+              {login && (
+                <Fragment>
+                  <strong>Username: </strong>
+                  {login}
+                </Fragment>
+              )}
+            </li>
+            <li>
+              {company && (
+                <Fragment>
+                  <strong>Company: </strong>
+                  {company}
+                </Fragment>
+              )}
+            </li>
+            <li>
+              {blog && (
+                <Fragment>
+                  <strong>Website: </strong>
+                  <a href={blog} target='_blank' rel='noreferrer noopener'>
+                    {blog}
+                  </a>
+                </Fragment>
+              )}
+            </li>
+          </ul>
         </div>
-      </Fragment>
-    );
-  }
-}
+      </div>
+      <div className='text-center'>
+        <div className='badge badge-primary'>Followers:{followers}</div>
+        <div className='badge badge-success'>following:{following}</div>
+        <div className='badge badge-light'>Public Repos:{public_repos}</div>
+        <div className='badge badge-dark'>Public Gists:{public_gists}</div>
+      </div>
+      <div>
+        <Repos repos={repos}></Repos>
+      </div>
+    </Fragment>
+  );
+};
+
+User.propsTypes = {
+  loading: PropTypes.bool,
+  user: PropTypes.object.isRequired,
+  getUser: PropTypes.func.isRequired,
+  getRepos: PropTypes.func.isRequired,
+  repos: PropTypes.array.isRequired
+};
 
 export default User;
